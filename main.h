cedfc52f7f5dbd8414a29c0aa55f4b73d85ae318
olajuwon lawal <olajuwonlawal2012@gmail.com>
2022-04-19 23:26:50 +0100

    defined struct for structprint

--- main.h
+++ main.h
@@ -5,3 +5,16 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+* struct structprint - structure containing the struct print
+* @q: the location and method to tranalte data to characters
+* @u: print function for specific data type
+* @format: the format of the data
+* Return int
+*/
+
+typedef struct structprint
+{
+    char *q;
+    int (*u)(char *format, va_list);
+} structtype;
