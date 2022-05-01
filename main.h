781195c757197f72de7ef8b28dbb8c176207018f
ol4juwon <olajuwonlawal2012@gmail.com>
2022-05-01 01:50:36 +0100

    evaluate function count

--- main.h
+++ main.h
@@ -26,6 +26,7 @@ int print_int(va_list arguments, char *buf, unsigned int ibuf);
 // int print_bnr(va_list arguments, char *buf, unsigned int ibuf);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
-
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+int ev_print_func(const char *s, int index);
 
 #endif
