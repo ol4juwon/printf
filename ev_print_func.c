a3c9eae23776c5c67b237e6a778937480c578e39
ol4juwon <olajuwonlawal2012@gmail.com>
2022-05-01 01:58:01 +0100

    Fixes to percentage

--- ev_print_func.c
+++ ev_print_func.c
@@ -11,7 +11,8 @@ int ev_print_func(const char *s, int index)
 	print_t pr[] = {
 		{"c", print_chr}, {"s", print_str}, {"i", print_int},
 		{"d", print_int}, {"#i", print_int},
-		{"#d", print_int},
+		{"#d", print_int}, {"%", print_prg}, {" %", print_prg},
+        {"l", print_prg}, {"h", print_prg},
 		{NULL, NULL},
 	};
 	int i = 0, j = 0, first_index;
