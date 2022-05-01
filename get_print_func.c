2699eae4fa9dc5d23b96c2be60358a05ecb0a214
ol4juwon <olajuwonlawal2012@gmail.com>
2022-05-01 02:11:47 +0100

    get functions

--- get_print_func.c
+++ get_print_func.c
@@ -10,10 +10,10 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
 	print_t pr[] = {
 		{"c", print_chr}, {"s", print_str},
 		{"i", print_int}, {"d", print_int},
-
-		{"#i", print_int}, {"#d", print_int},
+        {"#i", print_int}, {"#d", print_int},
         {"%", print_prg}, {"l", print_prg},
-        {"h", print_prg}, {" %", print_prg}, {NULL, NULL},
+        {"h", print_prg}, {" %", print_prg},
+        {NULL, NULL},
 	};
 	int i = 0, j = 0, first_index;
 
